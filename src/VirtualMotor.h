c0cee86a9719c3daa47c671d2bdb63490d344261
eleson2 <erland.lestander@gmail.com>
2025-11-28 21:15:18 +0100

    moved doc

--- src/VirtualMotor.h
+++ src/VirtualMotor.h
@@ -4,28 +4,39 @@
 #include "motor.hpp"
 #include "globals.hpp"
 
+/**
+ * VirtualMotor - Motor implementation for testing and debugging
+ *
+ * Simulates motor behavior without hardware:
+ * - Logs all commands to serial output (when DEBUG_ENABLED)
+ * - Tracks speed state internally
+ * - Use with DriveUnit for hardware-free testing
+ */
 class VirtualMotor : public Motor {
 public:
   VirtualMotor(const char* name = "Virtual") : _speed(0), _name(name) {}
+
   void move(int speed) override {
-    // store and optionally log
     _speed = constrain(speed, MOTOR_SPEED_MIN, MOTOR_SPEED_MAX);
     DEBUG_PRINT2(_name, ": move ");
     DEBUG_PRINTLN(_speed);
   }
+
   void stop() override {
     _speed = 0;
     DEBUG_PRINT2(_name, ": stop\n");
   }
+
   void reset() override {
     _speed = 0;
     DEBUG_PRINT2(_name, ": reset\n");
   }
+
   int getSpeed() override { return _speed; }
 
 private:
-  int _speed;
-  const char* _name;
+  int _speed;           // Current speed state (-1023 to +1023)
+  const char* _name;    // Motor identifier for debug output
 };
 
 #endif
